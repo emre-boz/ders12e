ead18d7942a207ea10e0dd1595d3d56dd549d3bf
Emre Boz <emrento@gmail.com>
2022-07-31 17:01:52 +0300

    React css kullanım örneklerine bakıldı.

---
+++ src/components/C1.jsx
@@ -0,0 +1,17 @@
+import './C1.css';
+
+const paragrafStili={
+    color: "red",
+    backgroundColor:"black",
+    padding: "1rem"
+}
+function C1() {
+
+    return (
+        <>
+            <p className='ozel-paragraf' style={paragrafStili}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora eligendi eum dolorum laboriosam, cupiditate delectus expedita molestias suscipit aperiam magni quae necessitatibus neque voluptates minima autem consequuntur sit sint omnis?</p>
+        </>
+    )
+}
+
+export default C1;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import C1 from './components/C1'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
-    <App />
+    <C1 />
   
 );
 
